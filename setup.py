d72f200cd394ef37cf2aa1b3f88e2e29b91adffb
subra <subratadas09072k4@gmail.com>
2025-11-15 20:56:20 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "med_chatbot",
+    version = "0.1.0",
+    author = "subrata das",
+    author_email = "subratadas09072k4@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
